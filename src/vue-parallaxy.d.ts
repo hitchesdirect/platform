62eec57105bb43a7bc4db71b793a6ff35508f176
Alex Ninneman <alex@ninneman.org>
2019-10-15 00:07:44 -0500

    adds workflow
    Signed-off-by: Alex Ninneman <alex@ninneman.org>

--- src/vue-parallaxy.d.ts
+++ src/vue-parallaxy.d.ts
@@ -1,18 +1,18 @@
 declare module 'vue-parallaxy' {
-    import Vue from 'vue';
-    export default class Parallax extends Vue {
-        message: string;
-        parallax: boolean;
-        speedFactor: number;
-        fixed: boolean;
-        breakpoint: string;
-        sectionHeight: number;
-        sectionClass: string;
-        containerClass: string;
-        parallaxClass: string;
-        fixedClass: string;
-        direction: string;
-    }
+	import Vue from 'vue';
+	export default class Parallax extends Vue {
+		public message: string;
+		public parallax: boolean;
+		public speedFactor: number;
+		public fixed: boolean;
+		public breakpoint: string;
+		public sectionHeight: number;
+		public sectionClass: string;
+		public containerClass: string;
+		public parallaxClass: string;
+		public fixedClass: string;
+		public direction: string;
+	}
 }
 
 declare module 'vue-carousel';
